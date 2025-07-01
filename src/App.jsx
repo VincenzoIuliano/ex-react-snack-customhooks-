fa9813701335292ca014c73e4134bf9fd254de27
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-07-01 17:28:48 +0200

    Milestone 2 solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import './App.css'
 import useSwitch from "./useSwitch";
+import useDate from "./useDate";
 
 function App() {
   const [isOn, toggle] = useSwitch();
+  const currentDate = useDate();
 
+  // SNACK 1
+
+  // return (
+  //   <div>
+  //     <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
+  //     <button onClick={toggle}>Cambia Stato</button>
+  //   </div>
+  // );
+
+  // SNACK 2
+  
   return (
     <div>
-      <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
-      <button onClick={toggle}>Cambia Stato</button>
+      <h1>Data e ora attuali:</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   );
+
 }
 
 export default App;
